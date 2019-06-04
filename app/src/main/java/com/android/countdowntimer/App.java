703f0d30e2fa3309380355322e1d08b5070ed2b4
siddworks <ultysagar@gmail.com>
2019-06-04 14:23:02 +0530

    Removed Fabric

--- app/src/main/java/com/android/countdowntimer/App.java
+++ app/src/main/java/com/android/countdowntimer/App.java
@@ -2,10 +2,6 @@ package com.android.countdowntimer;
 
 import android.app.Application;
 
-import com.crashlytics.android.Crashlytics;
-import com.crashlytics.android.answers.Answers;
-
-import io.fabric.sdk.android.Fabric;
 import io.paperdb.Paper;
 
 public class App extends Application {
@@ -13,7 +9,5 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         Paper.init(this);
-        Fabric.with(this, new Crashlytics());
-        Fabric.with(this, new Answers());
     }
 }
